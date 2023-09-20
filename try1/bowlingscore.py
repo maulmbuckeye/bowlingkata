3ec33e4c94cd7c490afa61d65984e5bc827e1df1
MIke Ulm <maulmbuckeye@gmail.com>
2023-09-20 11:48:59 -0400

    Make an OO-based design

--- try1/bowlingscore.py
+++ try1/bowlingscore.py
@@ -1,21 +1,72 @@
 
 def score(*b):
     bowls = list(b)
-    return scoring(bowls)
+    return Bowling(bowls).score()
 
 
-def scoring(bowls, frame=1):
-    if len(bowls) <= 1 or frame > 10:
+class Bowling:
+
+    def __init__(self, bowls, this_frame=1):
+        self.bowls = bowls
+        self.this_frame = this_frame
+        pass
+
+    def score(self):
+        if not self.bowls or self.this_frame > 10:
+            return []
+
+        if self.is_strike:
+            balls_to_score, balls_in_frame = 3, 1
+        elif self.is_spare:
+            balls_to_score, balls_in_frame = 3, 2
+        else:
+            balls_to_score, balls_in_frame = 2, 2
+
+        score_for_frame, remaining_bowls = self.how_to_score(balls_to_score, balls_in_frame)
+
+        next_frame = self.this_frame + 1
+        return [score_for_frame] + Bowling(remaining_bowls, next_frame).score()
+
+    def how_to_score(self, balls_to_score, balls_in_frame):
+        score_for_frame = self.score_next(balls_to_score)
+        remaining_bowls = self.bowls[balls_in_frame:]
+        return score_for_frame, remaining_bowls
+
+    @property
+    def is_spare(self):
+        return self.score_next(2) == 10
+
+    @property
+    def is_strike(self):
+        return self.score_next(1) == 10
+
+    def score_next(self, balls=3):
+        if len(self.bowls) < balls:
+            return 'open'
+        return sum(self.bowls[0:balls])
+
+
+def scoring(bowls, this_frame=1):
+
+    if len(bowls) == 0 or this_frame > 10:
         return []
-    next_frame = frame + 1
+    if len(bowls) == 1:
+        return ['open']
+    next_frame = this_frame + 1
     if bowls[0] == 10:
         remaining_bowls = bowls[1:]
-        this_frame = sum(bowls[0:3])
-        return [this_frame] + scoring(remaining_bowls, next_frame)
-    this_frame = bowls[0] + bowls[1]
-    remaining_bowls = bowls[2:]
-    if this_frame == 10:
-        this_frame += bowls[2]
+        if len(bowls) >= 3:
+            this_frame = sum(bowls[0:3])
+        else:
+            this_frame = 'open'
+    elif sum(bowls[0:2]) == 10:
+        remaining_bowls = bowls[2:0]
+        if len(bowls) >= 3:
+            this_frame = sum(bowls[0:3])
+        else:
+            this_frame = 'open'
+    else:
+        remaining_bowls = bowls[2:]
+        this_frame = sum(bowls[0:2])
+
     return [this_frame] + scoring(remaining_bowls, next_frame)
-
-
--- try1/test_bowlingscore.py
+++ try1/test_bowlingscore.py
@@ -7,7 +7,7 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual([], score())
 
     def test_partial_frame(self):
-        self.assertEqual([], score(4))
+        self.assertEqual(['open'], score(4))
 
     def test_complete_frame(self):
         self.assertEqual([9], score(4, 5))
@@ -16,7 +16,7 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual([9, 4], score(4, 5, 1, 3))
 
     def test_spare(self):
-        self.assertEqual([15], score(9,1,5))
+        self.assertEqual([15, 'open'], score(9,1,5))
 
     def test_strike(self):
         self.assertEqual([16, 6], score(10, 3, 3))
@@ -29,6 +29,15 @@ class MyTestCase(unittest.TestCase):
         bowls = 12 * [10]
         self.assertEqual(frames, score(*bowls))
 
+    def test_last_bowl_in_perfect(self):
+        frames = 9 * [30] + ['open']
+        bowls = 11 * [10]
+        self.assertEqual(frames, score(*bowls))
+
+    def test_all_spares_minus_one(self):
+        frames = 9 * [19] + ['open']
+        bowls = 10 * ([9, 1])
+        self.assertEqual(frames, score(*bowls))
 
 
 if __name__ == '__main__':
